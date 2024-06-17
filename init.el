316810a9b759ed038965edc076d8536190a8ac38
Kuro7s <kuro0970gamer@gmail.com>
2024-06-16 20:59:33 -0400

    wheatgrass is better imo

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; Theme
-(load-theme 'modus-vivendi)
+(load-theme 'wheatgrass)
 
 ;; Indentation
 ;; Mode specific indentation
